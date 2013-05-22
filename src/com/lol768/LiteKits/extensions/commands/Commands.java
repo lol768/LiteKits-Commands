ba2e984fc3640b9892fc5c2678b936899a1eafd1
lol768 <support@technostripe.com>
2013-05-22 14:57:48 +0100

    Add some debugging

--- src/com/lol768/LiteKits/extensions/commands/Commands.java
+++ src/com/lol768/LiteKits/extensions/commands/Commands.java
@@ -18,8 +18,10 @@ public class Commands extends JavaPlugin implements Listener {
             if (lk.getDescription().getVersion().equals("1.0")) {
                 getLogger().severe("LiteKits version is too old to use this extension. Disabling self...");
                 setEnabled(false);
+            } else {
+                Bukkit.getPluginManager().registerEvents(this, this);
             }
-            Bukkit.getPluginManager().registerEvents(this, this);
+            
         } else {
             getLogger().severe("Couldn't find LiteKits. Disabling self...");
             setEnabled(false);
@@ -31,23 +33,25 @@ public class Commands extends JavaPlugin implements Listener {
     @EventHandler
     public void onCommand(PlayerCommandPreprocessEvent e) {
         String kit = e.getMessage().substring(1);
+        getLogger().info("Got a command, it's " + kit);
         if (lk.kitExists(kit)) {
             if (!e.getPlayer().hasPermission("LiteKits.kit")) {
                 Messaging.sendPermissionsError(e.getPlayer(), lk.prefix);
+                getLogger().info("User doesn't have permission for LiteKits.kit");
                 e.setCancelled(true);
             } else {
                 if (!e.getPlayer().hasPermission("LiteKits.use." + kit)) {
                     Messaging.sendPermissionsError(e.getPlayer(), lk.prefix);
+                    getLogger().info("User doesn't have permission for LiteKits.use." + kit);
                     e.setCancelled(true);
                 } else {
                     lk.supplyKitToPlayer(kit, e.getPlayer());
+                    getLogger().info("Giving the kit to the user!");
                     e.setCancelled(true);
                 }
-            }
-            
-            
-            
-            
+            } 
+        } else {
+            getLogger().info("No such kit :(");
         }
     }
 
